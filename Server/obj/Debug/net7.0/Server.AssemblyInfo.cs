975f8c1f7932f1b26f49235dede258a7d84303a2
vailinhhon957 <23520640@gm.uit.edu.vn>
2025-05-24 20:55:50 +0700

    update server

--- Server/obj/Debug/net7.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net7.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dad5efb2e8e09dd220f27529ff9eb99a46584d42")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
